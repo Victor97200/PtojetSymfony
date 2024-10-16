93a430e0114988bcfea6586ebde8f9ea977192b8
Victor97200 <victorbuil11000@gmail.com>
2024-10-16 15:56:13 +0200

    fin tp1 - début tp2

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class BlogController extends AbstractController
+{
+    #[Route('/blog/list/{page}', name: 'blog_list', requirements: ['page' => '\d+']  ,defaults: ['page' => 1])]
+    public function listAction($page): Response
+    {
+        return new Response("<body>$page./body>");
+    }
+
+    #[Route('/blog/article/{idArticle}', name: 'blog_article', requirements: ['idArticle' => '\d+']  ,defaults: ['idArticle' => 1])]
+    public function viewAction($idArticle): Response
+    {
+        return new Response('<body>'.$idArticle.'</body>');
+    }
+
+    #[Route('/blog/article/add.', name: 'blog_add')]
+    public function addAction(): Response
+    {
+        if(false){
+            // Traitement du formulaire
+            // Message de succès
+            $this->addFlash('info', "Article ajouté avec succès!");
+            return $this->redirectToRoute('/blog/list');
+        }
+        return new Response('formulaire');
+    }
+
+    #[Route('/blog/article/edit/{idArticle}.', name: 'blog_edit', requirements: ['idArticle' => '\d+']  ,defaults: ['idArticle' => 1])]
+    public function editAction(): RedirectResponse
+    {
+        if(false){
+            // Traitement du formulaire
+            // Message de succès
+            $this->addFlash('info', "Article édité avec succès!");
+            return $this->redirectToRoute('/blog/list');
+        }
+        return new Response('formulaire');
+    }
+
+    #[Route(': /blog/article/delete/{idArticle}', name: 'blog_del', requirements: ['idArticle' => '\d+']  ,defaults: ['idArticle' => 1])]
+    public function deleteAction(): Response
+    {
+        if(false){
+            // Traitement du formulaire
+            // Message de succès
+            $this->addFlash('info', "Article supprimé avec succès!");
+            return $this->redirectToRoute('/blog/list');
+        }
+        return new Response('formulaire');
+    }
+
+
+}
