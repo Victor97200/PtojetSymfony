0c0d445984c9f1dac67349ecc2edc266282ff04c
Victor <victorbuil11000@gmail.com>
2024-11-04 14:19:53 +0100

    tst

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -12,50 +12,45 @@ class BlogController extends AbstractController
     #[Route('/blog/list/{page}', name: 'blog', requirements: ['page' => '\d+'], defaults: ['page' => 1])]
     public function listAction($page): Response
     {
-        return new Response("<body>$page</body>");
+        return $this->render('blog/list.html.twig', ['page' => $page]);
     }
 
     #[Route('/blog/article/{idArticle}', name: 'blog_article', requirements: ['idArticle' => '\d+'], defaults: ['idArticle' => 1])]
     public function viewAction($idArticle): Response
     {
-        return new Response("<body>$idArticle</body>");
+        return $this->render('blog/view.html.twig', ['idArticle' => $idArticle]);
     }
 
     #[Route('/blog/article/add', name: 'blog_add')]
     public function addAction(): Response
     {
         if (false) {
-            // Traitement du formulaire
-            // Message de succès
             $this->addFlash('info', "Article ajouté avec succès!");
             return $this->redirectToRoute('blog_list');
         }
-        return new response("formulaire");
+        return $this->render('blog/add.html.twig');
     }
 
     #[Route('/blog/article/edit/{idArticle}', name: 'blog_edit', requirements: ['idArticle' => '\d+'], defaults: ['idArticle' => 1])]
-    public function editAction(): Response
+    public function editAction($idArticle): Response
     {
         if (false) {
-            // Traitement du formulaire
-            // Message de succès
             $this->addFlash('info', "Article édité avec succès!");
             return $this->redirectToRoute('blog_list');
         }
-        return new Response("formulaire");
+        return $this->render('blog/edit.html.twig', ['idArticle' => $idArticle]);
     }
 
     #[Route('/blog/article/delete/{idArticle}', name: 'blog_del', requirements: ['idArticle' => '\d+'], defaults: ['idArticle' => 1])]
     public function deleteAction(): Response
     {
         if (false) {
-            // Traitement du formulaire
-            // Message de succès
             $this->addFlash('info', "Article supprimé avec succès!");
             return $this->redirectToRoute('blog_list');
         }
         return new Response('formulaire');
     }
+
     #[Route('/last-articles', name: 'last-articles')]
     public function lastArticlesAction($nbArticles): Response
     {
@@ -63,8 +58,7 @@ class BlogController extends AbstractController
         for ($i = 1; $i <= $nbArticles; $i++) {
             $articles[] = ['name' => 'Article ' . $i];
         }
-        return $this->render('blog/last_articles.html.twig',['articles' => $articles]);
+        return $this->render('blog/last_articles.html.twig', ['articles' => $articles]);
     }
-
 
 }
